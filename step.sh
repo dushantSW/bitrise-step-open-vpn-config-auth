ffbbc9d94baf9dcd9e98f076cdb7b186476a532e
Dushant  Singh <dushant.waora@konsultopedia.com>
2020-11-19 09:19:48 +0100

    Adds the steps script and steps input

---
+++ step.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -eu
+
+case "$OSTYPE" in
+  linux*)
+    echo "Configuring for Ubuntu"
+
+    echo ${client_config} | base64 -d > /etc/openvpn/client.ovpn
+    echo ${username} | base64 -d > /etc/openvpn/auth.txt
+    echo ${password} | base64 -d >> /etc/openvpn/auth.txt
+
+    sed -i '.bak' 's/auth-user-pass/auth-user-pass auth.txt/' /etc/openvpn/client.ovpn
+    service openvpn start client > /dev/null 2>&1
+    sleep 20
+
+    if ifconfig | grep tun0 > /dev/null
+    then
+      echo "VPN connection succeeded"
+    else
+      echo "VPN connection failed!"
+      exit 1
+    fi
+    ;;
+  darwin*)
+    echo "Configuring for Mac OS"
+
+    echo ${client_config} | base64 -D -o client.opvn > /dev/null 2>&1
+    echo ${username} | base64 -D -o > auth.txt
+    echo ${password} | base64 -D -o >> auth.txt
+
+    sed -i '.bak' 's/auth-user-pass/auth-user-pass auth.txt/' /etc/openvpn/client.ovpn
+    sudo openvpn --config client.opvn > /dev/null 2>&1 &
+    sleep 20
+
+    if ifconfig -l | grep utun0 > /dev/null
+    then
+      echo "VPN connection succeeded"
+    else
+      echo "VPN connection failed!"
+      exit 1
+    fi
+    ;;
+  *)
+    echo "Unknown operative system: $OSTYPE, exiting"
+    exit 1
+    ;;
+esac
